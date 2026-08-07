72135288dfd3d4c3f56be43f7dd5e36398287d41
Mansi <jn.mansi01@gmail.com>
2026-08-07 18:27:19 +0530

    Correct GTM container ID to GTM-NVQCNBSJ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NVQCNDSJ');
+            })(window,document,'script','dataLayer','GTM-NVQCNBSJ');
           `}
         </Script>
       </head>
@@ -49,7 +49,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Google Tag Manager (noscript) - Body fallback */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NVQCNDSJ"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NVQCNBSJ"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
